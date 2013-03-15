10ccce4daa149f969c8b1e5526de2a5e3dd503b7
Pim Schouten <pim-s@live.nl>
2013-03-15 09:42:52 +0100

    Pawn

--- BarricadeSpel/BarricadeSpel/Model/Pawn.cs
+++ BarricadeSpel/BarricadeSpel/Model/Pawn.cs
@@ -8,5 +8,41 @@ namespace BarricadeSpel.Model
 {
     class Pawn : Movable
     {
+        private Field position;
+        private String color;
+
+        public Field Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public String Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        // Returns the pawn to its designated position, if it were to be removed.
+        private void returnPawn()
+        {
+            if (position.ReturnTo == 0)
+                position = 0;//beginning
+            if (position.ReturnTo == 1)
+                position = 10;//forest
+        }
+
+        // Finishes the game
+        private void finishGame()
+        {
+            if(position == 100)//finish
+                Console.WriteLine("Gefeliciteerd "+color+"! Je hebt gewonnen!");
+        }
+
+        public Pawn(Field position, String color)
+        {
+            this.position = position;
+            this.color = color;
+        }
     }
 }
