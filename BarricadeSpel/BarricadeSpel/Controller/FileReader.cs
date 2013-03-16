2b992b151cbd504dd406d70696bf92e3b5ef1bda
Pim Schouten <pim-s@live.nl>
2013-03-16 11:12:49 +0100

    Made a FileReader
    It's still not worked out properly, has to be checked

---
+++ BarricadeSpel/BarricadeSpel/Controller/FileReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarricadeSpel.Controller
+{
+    class FileReader
+    {
+        /**
+         * Reads all lines in a file.
+         * @param domain: The domain name of the file.
+         */
+        public static void Read(string domain)
+        {
+            string[] lines = System.IO.File.ReadAllLines(domain);
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+                Console.WriteLine("\n" + line);
+        
+        }
+    }
+}
